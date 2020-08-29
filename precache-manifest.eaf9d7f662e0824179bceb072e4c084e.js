9a643a35334144e9dd8c5d6ae2ca09d7493c633c
Таня Бура <tanya.bura.99@gmail.com>
2020-08-29 11:55:11 +0300

    Updates

---
+++ precache-manifest.eaf9d7f662e0824179bceb072e4c084e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7141e50a92f1ad0907f618f342ad4d",
+    "url": "/jet_solutins_test/index.html"
+  },
+  {
+    "revision": "4281d17c3d5be2795426",
+    "url": "/jet_solutins_test/static/css/main.f9f287b5.chunk.css"
+  },
+  {
+    "revision": "af46f6fd53be8fdfa7ae",
+    "url": "/jet_solutins_test/static/js/2.58869b26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jet_solutins_test/static/js/2.58869b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4281d17c3d5be2795426",
+    "url": "/jet_solutins_test/static/js/main.b58cc8f7.chunk.js"
+  },
+  {
+    "revision": "2842e0fd3109512e5191",
+    "url": "/jet_solutins_test/static/js/runtime-main.dfe0a85e.js"
+  },
+  {
+    "revision": "fa3aaf3f7399cdc5be899828ea6e7582",
+    "url": "/jet_solutins_test/static/media/animal.fa3aaf3f.jpg"
+  },
+  {
+    "revision": "d6ac6edc70de7c814f48872e57cf0c95",
+    "url": "/jet_solutins_test/static/media/elephant.d6ac6edc.jpg"
+  },
+  {
+    "revision": "9aec3088e2e2e22ea5c1e41e52077afd",
+    "url": "/jet_solutins_test/static/media/fox.9aec3088.jpg"
+  },
+  {
+    "revision": "5355e6aa0ab2665bb884ada056ff7a5f",
+    "url": "/jet_solutins_test/static/media/horse.5355e6aa.jpg"
+  },
+  {
+    "revision": "dab9217067ea094eec0ed909d1114019",
+    "url": "/jet_solutins_test/static/media/lazy.dab92170.jpg"
+  },
+  {
+    "revision": "ca2b91ddd6a576745b49c9a8501fe437",
+    "url": "/jet_solutins_test/static/media/lion.ca2b91dd.jpg"
+  },
+  {
+    "revision": "9f1b96bdf0c7fb98b09d1180488fef45",
+    "url": "/jet_solutins_test/static/media/logo.9f1b96bd.jpg"
+  },
+  {
+    "revision": "3d4e6ed00a7473b81304f62ae84f457f",
+    "url": "/jet_solutins_test/static/media/panda.3d4e6ed0.jpg"
+  },
+  {
+    "revision": "98b09053e24ce48efc945a3830a3d2e2",
+    "url": "/jet_solutins_test/static/media/pingvi.98b09053.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/jet_solutins_test/precache-manifest.eaf9d7f662e0824179bceb072e4c084e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jet_solutins_test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
